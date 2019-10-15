eae7233f298298625b001bcbc3f3f84541ec3a08
Cameron Reikes <cameronreikes@gmail.com>
2019-10-15 16:24:12 -0700

    Add bullet recharge time and allow holding fire

--- src/Player.gd
+++ src/Player.gd
@@ -4,6 +4,7 @@ export var player_velocity = 500.0
 export var bullet_cooldown = 0.2
 
 var health: int = 5 setget set_health
+var cur_bullet_recharge_time: float = 0.0
 
 func _process(delta):
 	var horizontal: int = int(Input.is_action_pressed("g_right")) - int(Input.is_action_pressed("g_left"))
@@ -11,12 +12,15 @@ func _process(delta):
 	global_position.x += horizontal*player_velocity*delta
 	global_position.x = clamp(global_position.x,  $Sprite.texture.get_size().x/2, ProjectSettings.get_setting("display/window/size/width") - $Sprite.texture.get_size().x/2)
 	
-	if Input.is_action_just_pressed("g_fire"):
+	if Input.is_action_pressed("g_fire") and cur_bullet_recharge_time >= bullet_cooldown:
+		cur_bullet_recharge_time = 0.0
 		var cur_bullet = preload("res://Bullet.tscn").instance()
 		cur_bullet.to_ignore.append(self)
 		cur_bullet.global_position = global_position
 		cur_bullet.rotation = deg2rad(-90)
 		get_parent().add_child(cur_bullet)
+	
+	cur_bullet_recharge_time += delta
 
 func hit(damage: float):
 	self.health -= damage
